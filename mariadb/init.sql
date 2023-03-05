55badd3d2eab40fe1ace70d2650689a6f214949a
rezzahra <rezzahra@debian-BULLSEYE-live-builder-AMD64>
2023-03-05 23:54:21 +0100

    installating safe mysql works

--- mariadb/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress_password';
-INSERT INTO wp_users (user_login, user_pass, user_nicename, user_email, user_registered, user_status) VALUES ('user1', MD5('password1'), 'user1', 'user1@example.com', NOW(), 0);
-INSERT INTO wp_users (user_login, user_pass, user_nicename, user_email, user_registered, user_status) VALUES ('admin', MD5('password2'), 'admin', 'admin@example.com', NOW(), 0);
