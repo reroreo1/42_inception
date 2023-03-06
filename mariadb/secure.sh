1fd8bfc3dc347b53997c034af85799312b4056f8
Reroreo1 <86200459+skyjumper24@users.noreply.github.com>
2023-03-06 01:45:22 +0100

    creation of users and db

--- mariadb/secure.sh
+++ mariadb/secure.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
+service mysql start
 mysql --user=root <<_EOF_
-UPDATE mysql.user SET Password=PASSWORD('root123') WHERE User='root';
-DELETE FROM mysql.user WHERE User='root';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root123';
+DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+CREATE DATABASE IF NOT EXISTS WORDPRESS_DB;
+CREATE USER IF NOT EXISTS 'moderator'@'localhost' IDENTIFIED BY 'root123';
+GRANT ALL PRIVILEGES ON WORDPRESS_DB.* TO 'moderator'@'%';
 FLUSH PRIVILEGES;
 _EOF_
